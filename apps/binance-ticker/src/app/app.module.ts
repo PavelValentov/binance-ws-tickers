0928a1de63364b43fa59807e892ff3b9cd6d1020
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2023-11-08 12:38:28 +0300

    Enable Binance Feeder

--- apps/binance-ticker/src/app/app.module.ts
+++ apps/binance-ticker/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { IntraModule } from '@exchanges/intra';
+import { LibsWsExchangeModule } from '@exchanges/ws-exchange';
 import { PrismaClientModule } from '@exchanges/prisma-client';
+import { IntraModule } from '@exchanges/intra';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [IntraModule, PrismaClientModule],
+  imports: [IntraModule, PrismaClientModule, LibsWsExchangeModule],
   controllers: [AppController],
   providers: [AppService],
 })
